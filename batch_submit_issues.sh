4b62ebd84961e12a1c84c503d275c3272db55900
Dave Beck <dacb@uw.edu>
2019-01-25 13:26:50 -0800

    added support for supplying issue title by an argument and checking and skipping a dir if the ISSUE_TEMPLATE.md file is missing

--- batch_submit_issues.sh
+++ batch_submit_issues.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-# Change variable 'title' to set a common name of all issues.
-title="SEDS HW 1 Grade"
+#
+#
+#
+
+# set variable 'title' to set a common name of all issues.
+if [ "$1" != "" ]; then
+	title="$1"
+	echo "using '$title' as the issue title"
+else
+	echo "usage: $0 <issue title>"
+	exit 1
+fi
+
+# get a temporary file for preparing the issue text
+tmpfile=$(mktemp /tmp/bash_submit_issues.sh.XXXXXXXXXX) || { echo "failed to create a tmp file"; exit 1; }
 
 # Issues will be sent for all github repos (homework repos) in this parent directory.
 # Alternatively, you can set a leading path to all the homework repos on your local machine.
 hwdir="`PWD`/*/"
 
 # loop through each student's homework repo in this directory
-for i in $hwdir
+for dir in $hwdir
 do
 
-echo $i
-cd $i
+	echo $dir
+	cd $dir
 
-# A temporary file is needed to specify both issue title and content at once.
-# The title needs to be the first line separated by an empty line from content.
-echo $title > temp_issue_header.txt
-echo "" >> temp_issue_header.txt
-cat ISSUE_TEMPLATE.md >> temp_issue_header.txt
+	if [ ! -e ISSUE_TEMPLATE.md ]; then
+		echo "WARNING: unable to find an ISSUE_TEMPLATE.md file, skipping this directory..."
+		continue
+	fi
 
-# removing the temporary file
-`mv temp_issue_header.txt ISSUE_TEMPLATE.md`
+	# A temporary file is needed to specify both issue title and content at once.
+	# The title needs to be the first line separated by an empty line from content.
+	echo $title > $tmpfile
+	echo "" >> $tmpfile
+	cat ISSUE_TEMPLATE.md >> $tmpfile
 
-# creating the issue, url of new issue will be copied to your computer clipboard
-# using the alternative command will not copy to your clipboard, but will print an error line insted
-`hub issue create -c -F ISSUE_TEMPLATE.md`
-#`hub issue create -F ISSUE_TEMPLATE.md` # alternative command
+	# creating the issue, url of new issue will be copied to your computer clipboard
+	# using the alternative command will not copy to your clipboard, but will print an error line insted
+	#hub issue create -c -F $tmpfile
+	cat $tmpfile
 
+	# removing the temporary file
+	rm $tmpfile
 done
 
