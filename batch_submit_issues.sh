200baa90f3f4356989d10d878ff29339a0125a96
Dave Beck <dacb@uw.edu>
2019-01-25 13:28:05 -0800

    added a comment on the script behavior

--- batch_submit_issues.sh
+++ batch_submit_issues.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #
-#
+# This script will batch submit the homeowrk grading issues for all github 
+# repos (student homework repos) in the current path. This is to be used 
+# after you have graded all the homework repos on your local machine by 
+# modifying the ```ISSUE_TEMPLATE.md``` file that is automatically generated 
+# in the homework repos. It eliminates the need to manually create an issue 
+# and fill out the grade online in github/github classroom. 
 #
 
 # set variable 'title' to set a common name of all issues.
